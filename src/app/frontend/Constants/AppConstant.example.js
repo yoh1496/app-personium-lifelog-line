cea333e516e2811be18b1f4c8c20e8a561b8e8e5
ishiguro.yo <yoh1496@gmail.com>
2020-11-25 08:56:56 +0000

    initial commit

--- src/app/frontend/Constants/AppConstant.example.js
+++
@@ -1,5 +0,0 @@
-export const AppConstant = {
-  cellUrl: '<CELL_URL>',
-  installBoxName: '<BOX_NAME>',
-  barFileUrl: '<BAR_FILE_URL>',
-};
---
+++ src/app/frontend/Constants/AppConstant.js
@@ -0,0 +1,5 @@
+export const AppConstant = {
+  cellUrl: 'https://app-ishiguro-01.appdev.personium.io/',
+  installBoxName: 'app-ishiguro-01',
+  barFileUrl: 'https://app-ishiguro-01.appdev.personium.io/app-ishiguro-01.bar',
+};
